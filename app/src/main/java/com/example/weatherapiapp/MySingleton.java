379e8c1da9ae045639cec0e69fffacf2740cd55f
Moira Leon <moira.leon2@gmail.com>
2021-10-13 19:11:36 -0600

    adding correct code

--- app/src/main/java/com/example/weatherapiapp/MySingleton.java
+++ app/src/main/java/com/example/weatherapiapp/MySingleton.java
@@ -1,50 +1,61 @@
 package com.example.weatherapiapp;
 
-import com.android.volley.Cache;
-import com.android.volley.Network;
+import android.content.Context;
+import android.graphics.Bitmap;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
 
 public class MySingleton {
-    RequestQueue requestQueue;
+    private static MySingleton instance;
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+    private static Context ctx;
 
-    // Instantiate the cache
-    Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+    private MySingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
 
-    // Set up the network to use HttpURLConnection as the HTTP client.
-    Network network = new BasicNetwork(new HurlStack());
+        imageLoader = new ImageLoader(requestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
 
-// Instantiate the RequestQueue with the cache and network.
-    requestQueue = new RequestQueue(cache, network);
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
 
-// Start the queue
-requestQueue.start();
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
 
-    String url ="http://www.example.com";
+    public static synchronized MySingleton getInstance(Context context) {
+        if (instance == null) {
+            instance = new MySingleton(context);
+        }
+        return instance;
+    }
 
-    // Formulate the request and handle the response.
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Do something with the response
-                }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    // Handle error
-                }
-            });
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
 
-// Add the request to the RequestQueue.
-requestQueue.add(stringRequest);
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
 
-// ...
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
 }
